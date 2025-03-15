87dd4e86eb50cf5efd9dffb8d53977b7318b7bb1
Miwa / Ensan <ensan.hcl.dev@gmail.com>
2025-03-16 00:49:10 +0900

    fix: surpress warning

--- AzooKeyCore/Sources/SwiftUIUtils/IdentifiableItems.swift
+++ AzooKeyCore/Sources/SwiftUIUtils/IdentifiableItems.swift
@@ -9,7 +9,7 @@
 import Foundation
 import SwiftUI
 
-public extension Binding where Value: RandomAccessCollection & MutableCollection, Value.Element: Identifiable {
+public extension Binding where Value: RandomAccessCollection & MutableCollection & Sendable, Value.Element: Identifiable, Value.Index: Sendable {
     struct IdentifiableItem: Identifiable {
         @Binding<Value.Element> public private(set) var item: Value.Element
         public let index: Value.Index
@@ -21,11 +21,14 @@ public extension Binding where Value: RandomAccessCollection & MutableCollection
         // そこでここでは`Binding`を生成し直すことで対処している。今のところうまく行っている。
         self.wrappedValue.indices.map { i in
             .init(
-                item: .init {
-                    self.wrappedValue[i]
-                } set: {newValue in
-                    self.wrappedValue[i] = newValue
-                },
+                item: .init(
+                    get: {
+                        self.wrappedValue[i]
+                    },
+                    set: {newValue in
+                        self.wrappedValue[i] = newValue
+                    }
+                ),
                 index: i,
                 id: self.wrappedValue[i].id
             )
